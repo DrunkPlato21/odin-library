03adc359728ce0d2d135afdacbe7362da6633079
Clay <claytrainor@ymail.com>
2022-05-10 23:29:40 -0400

    HTML template added with forms hidden, javascript started

--- library.js
+++ library.js
@@ -1,9 +1,27 @@
+//declare empty array for library
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+
+//Book Constructor
+function Book(author, title, pages, read) {
+
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.read = read
+    this.info = function(){
+        return (title + author + ", " + pages)
+    }
+
 }
+
+//add new book to myLibrary array
 
 function addBookToLibrary() {
   // do stuff here
+
 }
+
+let x = new Book("Ben", "Title", 39, true)
+
+console.log(x.author)
