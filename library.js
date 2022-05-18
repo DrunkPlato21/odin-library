48d958b35b462f6afab01e14f437a6bd4e08179d
Clay <claytrainor@ymail.com>
2022-05-18 15:15:17 -0400

    updated the html, added some javascript

--- library.js
+++ library.js
@@ -12,11 +12,19 @@ function Book(author, title, pages, read) {
 
 }
 
-//add new book to myLibrary array
 
+//grab form data
+let bookTitle = document.getElementById('Title').value
+let bookAuthor = document.getElementById('Author').value
+let bookPages = document.getElementById('Pages').value
+let bookRead = document.getElementById('Read').value
+
+//add new book to myLibrary array
 function addBookToLibrary() {
   // do stuff here
-
+    let newBook = new Book(bookAuthor, bookTitle, bookPages, bookRead)
+    myLibrary.push(newBook)
+    console.log(newBook.author)
 }
 
 
@@ -24,12 +32,25 @@ function addBookToLibrary() {
 const addBookButton = document.querySelector(".add-book-button");
 addBookButton.addEventListener("click", displayForm);
 
+//close form button
+const closeBookForm = document.querySelector(".close-form");
+closeBookForm.addEventListener("click", closeForm)
+
+//close form function
+function closeForm(){
+    document.getElementById("add-book-form").style.display = "none";
+}
 
 //display the form
 function displayForm(){
     document.getElementById("add-book-form").style.display = "";
 }
 
-let x = new Book("Ben", "Title", 39, true)
+// start event listener for adding input from form
 
-console.log(x.author)
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", submitForm)
+
+function submitForm(){
+    addBookToLibrary()
+}
