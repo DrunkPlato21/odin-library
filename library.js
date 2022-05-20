89607a6eac55da7e83d7c725b2751a0ef90e77c1
Clay <claytrainor@ymail.com>
2022-05-20 16:55:40 -0400

    added logic for adding books to array

--- library.js
+++ library.js
@@ -13,18 +13,19 @@ function Book(author, title, pages, read) {
 }
 
 
-//grab form data
+//grab initial form data on page start
 let bookTitle = document.getElementById('Title').value
 let bookAuthor = document.getElementById('Author').value
 let bookPages = document.getElementById('Pages').value
 let bookRead = document.getElementById('Read').value
+
 
 //add new book to myLibrary array
 function addBookToLibrary() {
   // do stuff here
     let newBook = new Book(bookAuthor, bookTitle, bookPages, bookRead)
     myLibrary.push(newBook)
-    console.log(newBook.author)
+    console.log(myLibrary)
 }
 
 
@@ -37,7 +38,8 @@ const closeBookForm = document.querySelector(".close-form");
 closeBookForm.addEventListener("click", closeForm)
 
 //close form function
-function closeForm(){
+function closeForm(event){
+    event.preventDefault(); 
     document.getElementById("add-book-form").style.display = "none";
 }
 
@@ -51,6 +53,28 @@ function displayForm(){
 const submitButton = document.querySelector(".submit-button");
 submitButton.addEventListener("click", submitForm)
 
-function submitForm(){
+function submitForm(event){
+
+    //grab updated form data
+    bookTitle = document.getElementById('Title').value
+    bookAuthor = document.getElementById('Author').value
+    bookPages = document.getElementById('Pages').value
+    bookRead = document.getElementById('Read').value
+
+    //prevent form action
+    event.preventDefault(); 
+
     addBookToLibrary()
 }
+
+addBookToLibrary()
+addBookToLibrary()
+addBookToLibrary()
+
+function displayBooks(){
+ myLibrary.forEach(function (item, index){
+     console.log(item, index)
+ })
+}
+
+displayBooks()
