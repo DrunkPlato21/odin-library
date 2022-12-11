689c1bce6910d03d70b2c283c13a397fe2e5d079
Clay <claytrainor@ymail.com>
2022-12-10 20:57:40 -0500

    updated

--- library.js
+++ library.js
@@ -2,50 +2,144 @@ let myLibrary = [];
 let bookName
 let author
 let pages
+let counter
+let isRead
 
+let elements = document.querySelectorAll('.del')
 
 function getFormValues(){
 
      bookName = document.getElementById('name').value
      author = document.getElementById('author').value
      pages = document.getElementById('pages').value
+     isRead = document.getElementById('read-toggle').checked
 
 }
 
-function Book(name, author, pages) {
-  // the constructor...
+function Book(name, author, pages, isRead, index) {
   this.name = name
   this.author = author
   this.pages = pages
+  this.index = index
+  this.isRead = isRead
 }
 
-function addBookToLibrary(title, author, pages) {
-  // do stuff here
+// Add a prototype method to the Book constructor
 
-  let bookDiv = document.createElement('div')
-  bookDiv.classList.add('book')
-  let bookTitle = document.createElement('div')
-  bookTitle.classList.add('book-title')
-  let bookAuthor = document.createElement('div')
-  bookAuthor.classList.add('book-author')
-  let bookPages = document.createElement('div')
-  bookPages.classList.add('book-pages')
+Book.prototype.toggleReadStatus = function() {
+  this.isRead = !this.isRead;
+  
+}
 
-  bookTitle.textContent = title
-  bookAuthor.textContent = author
-  bookPages.textContent = pages
 
-  bookDiv.append(bookTitle)
-  bookDiv.append(bookAuthor)
-  bookDiv.append(bookPages)
 
-  let libraryContainer = document.querySelector('.library-container')
-  libraryContainer.append(bookDiv)
+function addBookToLibrary(title, author, pages, isRead, index) {
+
+  
+//grab values from form
+  
+let bookDiv = document.createElement('div')
+let bookTitle = document.createElement('div')
+let bookAuthor = document.createElement('div')
+let bookPages = document.createElement('div')
+
+        bookDiv.classList.add('book')
+        bookTitle.classList.add('book-title')
+        bookAuthor.classList.add('book-author')
+        bookPages.classList.add('book-pages')
+
+        //create book object and add it to array
+
+        let newBook = new Book(title, author, pages, isRead, myLibrary.length)
+        myLibrary.push(newBook)
+        // console.log(myLibrary)
+
+        refreshGrid()
 
 }
 
 function refreshGrid(){
-  
+  counter = 0
+
+  // empty grid
+  const libraryContainer = document.querySelector('.library-container')
+  libraryContainer.innerHTML = ""
+
+
+  for (let i = 0; i < myLibrary.length; i++){
+    
+    let bookDiv = document.createElement('div')
+    let bookTitle = document.createElement('div')
+    let bookAuthor = document.createElement('div')
+    let bookPages = document.createElement('div')
+    
+    //create delete button
+    let delDiv = document.createElement('div')
+    delDiv.classList.add("book-buttons")
+    let delButton = document.createElement('div')
+    delButton.classList.add('del')
+    delButton.setAttribute('index', i)
+    
+
+    delButton.addEventListener('click', function(e){
+
+      let delIndex = delButton.getAttribute('index')
+      myLibrary.splice(delIndex, 1)
+      refreshGrid()
+
+    } )
+
+    // create toggle switch
+
+const readToggle = document.createElement('input');
+readToggle.setAttribute('type', 'checkbox');
+readToggle.setAttribute('class', 'read-toggle');
+readToggle.setAttribute('index', i)
+
+if(myLibrary[i]["isRead"] === true){
+  readToggle.checked = true
+} else{
+  readToggle.checked = false
+}
+
+
+
+readToggle.addEventListener('click', function(e){
+
+  let readIndex = readToggle.getAttribute('index')
+  myLibrary[readIndex].toggleReadStatus()
+  console.log(myLibrary[readIndex].isRead)
+
+})
+
+delDiv.append(readToggle)
+delDiv.append(delButton)
+
+
+    bookDiv.classList.add('book')
+    bookTitle.classList.add('book-title')
+    bookAuthor.classList.add('book-author')
+    bookPages.classList.add('book-pages')
+
+    bookTitle.textContent = myLibrary[i]['name']
+    bookAuthor.textContent = myLibrary[i]['author']
+    bookPages.textContent = myLibrary[i]['pages'] + " pages"
+
+    bookDiv.append(bookTitle)
+    bookDiv.append(bookAuthor)
+    bookDiv.append(bookPages)
+    bookDiv.append(delDiv)
+
+    
+    libraryContainer.append(bookDiv)
+    console.log()
+
+    
+    counter++
+
+  }
+  elements = document.querySelectorAll('.del')
+  saveBooks()
 }
 
 let form = document.querySelector('.submit-form');
@@ -56,13 +150,17 @@ function showForm() {
 
 
 
+///BUTTONS *************************************
+
+
 let submit = document.getElementById('submit')
 
 submit.addEventListener("click", (event)=>{
 
   event.preventDefault()
   getFormValues()
-  addBookToLibrary(bookName, author, pages)
+
+  addBookToLibrary(bookName, author, pages, isRead)
   form.classList.remove('show');
    
 })
@@ -71,15 +169,25 @@ submit.addEventListener("click", (event)=>{
 
 let closeButton = document.querySelector('.close-button');
 
-closeButton.addEventListener('click', function() {
-  form.classList.remove('show');
+  closeButton.addEventListener('click', function() {
+    form.classList.remove('show');
 
 });
 
+function saveBooks(){
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
+function loadBooks(){
+  myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  refreshGrid()
+}
+
+loadBooks()
+
+// addBookToLibrary("Lord of the Rings", "J.R.R Tolkien", "1200", true)
+// addBookToLibrary("Game of Thrones", "GRRM", "1200", false)
+// addBookToLibrary("The Bible", "God", "2000", false)
 
 
-addBookToLibrary("it", "worked", "brosephina")
-
-
-
-console.log(myLibrary)
+// console.log(myLibrary)
